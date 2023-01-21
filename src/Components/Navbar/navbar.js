36eae8da976a35c086a279adb7129093cae78163
Osorabrian <osorabrian@gmail.com>
2023-01-21 06:05:25 +0300

    create app components folders and files and navbar component

---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar(){
+    return (
+        <nav className="navbar bg-success">
+            <div className="container-fluid">
+                <i className="fa-brands fa-bots"></i>
+                <a className="navbar-brand me-auto text-light" href="#">Bot Battlr</a>
+            </div>
+        </nav>
+    )
+}
